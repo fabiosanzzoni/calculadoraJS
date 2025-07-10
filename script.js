38eb46af2d0cf020e46c6812ccc982434d428a4b
Fábio Sanzzoni <168327984+fabiosanzzoni@users.noreply.github.com>
2025-07-10 17:24:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+alert("Não será possível obter o resto da divisão caso o primeiro número seja menor que o segundo!")
+numeroUm = Number(prompt("Por favor digite o primeiro número!"))
+numeroDois = Number(prompt("Por favor digite o segundo número!"))
+
+soma = numeroUm + numeroDois
+subtracao = numeroUm - numeroDois
+multiplicacao = numeroUm * numeroDois
+divisao = numeroUm / numeroDois
+resto = numeroUm % numeroDois
+
+console.log(`A soma entre o ${numeroUm} e o ${numeroDois} é ${soma}`)
+console.log(`A subtracao entre o ${numeroUm} e o ${numeroDois} é ${subtracao}`)
+console.log(`A multiplicacao entre o ${numeroUm} e o ${numeroDois} é ${multiplicacao}`)
+console.log(`A divisao entre o ${numeroUm} e o ${numeroDois} é ${divisao}`)
+console.log(`O resto da divisão entre o ${numeroUm} e o ${numeroDois} é ${rest}`)
